0c0863e2839a5b5a96c7dc6f18c30c9d4efea50f
hudajundi <huda.aljundi97@gmail.com>
2020-11-18 12:02:08 +0200

    exam

---
+++ data/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE harryP (
+    id SERIAL PRIMARY KEY NOT NULL ,
+    name TEXT,
+    house TEXT,
+)
--- server.js
+++ server.js
@@ -43,17 +43,94 @@ app.set('view engine', 'ejs');
 // ----------------------
 // ------- Routes -------
 // ----------------------
+app.get('/home', mainfunction);
+app.post('/house_name/characters', addHouse);
+app.get('/characters', showResult);
+app.get('/my-characters/:id', showSpecificRes);
+app.put('/character/character/:id', updateResult);
+app.delete('/character/character/:id', deleteResult);
 
 
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
+function mainfunction(req,res) {
+    let url='https://hp-api.herokuapp.com/';
+    superagent.get(url).then(result =>{
+        let house = result.body.map(item=>{
+            return new House(item);
+        })
+        res.render('home-page', {houseKey : house })
+    })
+}
+
+// cons: 
+function House (info){
+    this.charName= info.name;
+    this.charHouse= info.house;
+}
 
 
+function addHouse(req,res){
+    let charName = req.body;
+    let charHouse = req.body;
 
+    let SQL= 'INSERT INTO harryP VALUES ($1, $2); ';
+    let value = [charName, charHouse];
+
+    client.query(SQL, value).then(()=>{
+        res.redirect('/characters');
+    })
+}
+
+
+function showResult(req,res){
+    let SQL= 'SELECT * FROM harryP;';
+    let value = [charName, charHouse];
+
+    client.query(SQL).then(result =>{
+        res.render('fav-page', ({favResult : result.rows}))
+
+    })
+}
+
+function showSpecificRes(req,res){
+    let id= req.params.id;
+    let SQL = `SELECT * FROM harryP WHERE id=${id};`; 
+    
+    client.query(SQL).then(result=>{
+        res.render('fav-page', ({favResult : result.rows}) );
+    })
+
+}
 // -----------------------------------
 // --- CRUD Pages Routes functions ---
 // -----------------------------------
+
+/////The delete and the update: 
+function updateResult(req,res){
+    let id = req.params.id;
+
+    let charName= req.body;
+    let charHouse= req.body;
+
+    let SQL = `UPDATE harryP SET $1, $2 WHERE id=${id};`;
+    let value= [charName, charHouse];
+
+    client.query(SQL , value).then(result=>{
+        res.render('detail-page', ({favResult : result.rows}))
+    }) 
+}
+
+
+function deleteResult(req,res){
+    let id = req.params.id;
+
+    let SQL =`DELETEFROM harryP WHERE id=${id};`;
+
+    client.render(SQL).then('detail-page', ({favResult : result.rows}))
+}
+
 
 
 
